eba3d198f157dd5710e09fd6e895604630036bc8
MutatedTarsier <issac04032004@gmail.com>
2025-03-04 21:47:58 -0800

    Removed initialization tests for edges and overwrites
    Added tests for getting edges, and fixed errors

--- src/test/java/PyramidRubiksCubeTest.java
+++ src/test/java/PyramidRubiksCubeTest.java
@@ -12,7 +12,7 @@ class PyramidRubiksCubeTest {
 
     @Test
     void testInitializeFaces() {
-        pyramidRubiksCube.initializeFaces();
+
         char[][][] pyramid = pyramidRubiksCube.getPyramid();
         char[][][] expected = new char[][][]{
                 new char[][] {
@@ -39,104 +39,24 @@ class PyramidRubiksCubeTest {
         assertArrayEquals(expected, pyramid);
     }
 
-    @Test
-    void testInitializeEdges() {
-        pyramidRubiksCube.initializeEdges();
-        char[][][] pyramid = pyramidRubiksCube.getPyramid();
-        char[][][] expected = new char[][][]{
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'r','0','r'},
-                        new char[] {'0','0','r','0','0'},
-                },
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'g','0','g'},
-                        new char[] {'0','0','g','0','0'},
-                },
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'b','0','b'},
-                        new char[] {'0','0','b','0','0'},
-                },
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'y','0','y'},
-                        new char[] {'0','0','y','0','0'},
-                }
-        };
-        assertArrayEquals(expected, pyramid);
-    }
-
-    @Test
+       @Test
     void testValidateCubeFaces() {
-        pyramidRubiksCube.initializeFaces();
         assertTrue(pyramidRubiksCube.validateCube());
     }
 
     @Test
-    void testValidateCubeEdges() {
-        pyramidRubiksCube.initializeEdges();
-        assertFalse(pyramidRubiksCube.validateCube());
+    void testGetEdges() {
+        char[][] expected = new char[][] {
+                new char[] {'r', 'b'},
+                new char[] {'r', 'y'},
+                new char[] {'g', 'r'},
+                new char[] {'g', 'y'},
+                new char[] {'b', 'g'},
+                new char[] {'b', 'y'}
+        };
+        assertArrayEquals(expected, pyramidRubiksCube.getEdges());
+
     }
 
-    @Test
-    void testInitializeOverrideFaces(){
-        pyramidRubiksCube.initializeFaces();
-        pyramidRubiksCube.initializeEdges();
-        char[][][] pyramid = pyramidRubiksCube.getPyramid();
-        char[][][] expected = new char[][][]{
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'r','0','r'},
-                        new char[] {'0','0','r','0','0'},
-                },
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'g','0','g'},
-                        new char[] {'0','0','g','0','0'},
-                },
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'b','0','b'},
-                        new char[] {'0','0','b','0','0'},
-                },
-                new char[][] {
-                        new char[] {'0'},
-                        new char[] {'y','0','y'},
-                        new char[] {'0','0','y','0','0'},
-                }
-        };
-        assertArrayEquals(expected, pyramid);
-    }
 
-    @Test
-    void testInitializeOverrideEdges(){
-        pyramidRubiksCube.initializeEdges();
-        pyramidRubiksCube.initializeFaces();
-        char[][][] pyramid = pyramidRubiksCube.getPyramid();
-        char[][][] expected = new char[][][]{
-                new char[][] {
-                        new char[] {'r'},
-                        new char[] {'r','r','r'},
-                        new char[] {'r','r','r','r','r'},
-                },
-                new char[][] {
-                        new char[] {'g'},
-                        new char[] {'g','g','g'},
-                        new char[] {'g','g','g','g','g'},
-                },
-                new char[][] {
-                        new char[] {'b'},
-                        new char[] {'b','b','b'},
-                        new char[] {'b','b','b','b','b'},
-                },
-                new char[][] {
-                        new char[] {'y'},
-                        new char[] {'y','y','y'},
-                        new char[] {'y','y','y','y','y'},
-                }
-        };
-        assertArrayEquals(expected, pyramid);
-    }
 }
